cc5bb1f42923d9282c4af605f2a065da5e439cd8
Thomas Freeborough <thomas@hotsnapper.com>
2017-09-30 16:14:21 +0100

    [Welcome] Change welcome screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Rishadan</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -49,14 +49,9 @@
                 font-size: 84px;
             }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
+            .subtitle{
+                font-size: 24px;
+                margin:0;
             }
 
             .m-b-md {
@@ -79,15 +74,10 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Rishadan
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="subtitle m-b-md">
+                    <p>Coming Soon.</p>
                 </div>
             </div>
         </div>

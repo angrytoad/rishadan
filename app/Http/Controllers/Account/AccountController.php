fd6d5e0e8e1271e4b6645c9e3c268081cb98af1d
Thomas Freeborough <thomas@hotsnapper.com>
2017-10-04 02:28:39 +0100

    [Account] Account page

---
+++ app/Http/Controllers/Account/AccountController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Http\Controllers\Account;
+
+use App\Http\Controllers\Controller;
+
+class AccountController extends Controller
+{
+
+    public function index()
+    {
+        return view('layouts.account.index');
+    }
+
+}

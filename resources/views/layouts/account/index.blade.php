b62c8997a129e906befc3794b24e686619d3c681
Thomas Freeborough <thomas@hotsnapper.com>
2017-10-10 22:24:24 +0100

    [Account] Account index page styling

--- resources/views/layouts/account/index.blade.php
+++ resources/views/layouts/account/index.blade.php
@@ -2,17 +2,43 @@
 
 @section('content')
     <div id="account" class="main">
-        <div id="address_settings">
-            <h4 class="subtitle">Your Saved Addresses</h4>
-        </div>
-        <div id="account_settings">
-            <h4 class="subtitle">Account Settings</h4>
-        </div>
-        <div id="last_login">
-
-        </div>
-        <div id="profile_information">
-            <h4 class="subtitle">Profile Information</h4>
+        @include('includes.flash_message')
+        <div id="account_blocks_wrapper">
+            <div id="address_settings">
+                <h4 class="subtitle">My Addresses</h4>
+                <div id="address_settings_wrap">
+                    @foreach(Auth::user()->addresses as $address)
+                        <a href="/me/account/address/{{$address->id}}">
+                            <div class="address">
+                                <div class="address_cell">
+                                    <p>
+                                        {{ $address->first_line }}, {{ $address->second_line }}
+                                    </p>
+                                    <p>
+                                        {{ $address->postcode }}
+                                    </p>
+                                </div>
+                            </div>
+                        </a>
+                    @endforeach
+                    @if(count(Auth::user()->addresses) === 0)
+                        <a href="{{ route('account.add_address') }}">
+                            <div class="address">
+                                <div class="address_cell">
+                                    <i class="fa fa-plus" aria-hidden="true"></i>
+                                    <p>Add a new address</p>
+                                </div>
+                            </div>
+                        </a>
+                    @endif
+                </div>
+            </div>
+            <div id="account_settings">
+                <h4 class="subtitle">My Account</h4>
+            </div>
+            <div id="profile_information">
+                <h4 class="subtitle">My Profile</h4>
+            </div>
         </div>
     </div>
 @endsection

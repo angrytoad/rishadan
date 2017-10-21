5172e8bdc3c110fd514188088cdd8cfc4c990d13
Thomas Freeborough <thomas@hotsnapper.com>
2017-10-21 02:35:21 +0100

    [Importer] Added some comments

--- app/Console/Commands/CardImporter.php
+++ app/Console/Commands/CardImporter.php
@@ -57,12 +57,23 @@ class CardImporter extends Command
 
         /*
          * Uncomment/Comment these are you need them these as you need them
+         *
+         * Anything that needs important can be added into a separate method, for example, importing cards can go into a method, but
+         * when linking across to colours and rarities please put them into a different method so they don't end up massive so its easy
+         * to change at a later date.
+         *
+         * Happy Coding!
          */
+
         $this->setImporter();
     }
 
+
     public function setImporter()
     {
+        /*
+         * This probably doesn't need anything else doing unless you are feeling brave and want to try linking it into blocks.
+         */
         $this->info('---------- Doing Set Imports ----------');
         foreach($this->json as $set){
             $row = Set::where('name','=',$set["name"])->first();

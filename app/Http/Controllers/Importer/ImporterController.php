a3bf2c85a143df65b7e8c9ddaffdf98de41de156
Thomas Freeborough <thomas@hotsnapper.com>
2017-10-22 02:44:07 +0100

    [Importer] Add controller to view importer stuff

--- app/Http/Controllers/Importer/ImporterController.php
+++ app/Http/Controllers/Importer/ImporterController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Importer;
 
-ini_set('memory_limit','512M');
 
 use App\Http\Controllers\Controller;
 
@@ -11,8 +10,9 @@ class ImporterController extends Controller
 
     public function show()
     {
-        $json = json_decode(file_get_contents(base_path().'/storage/importer/extracted/AllSets-x.json'), true);
-        dd($json["JOU"]);
+        //$json = json_decode(file_get_contents(base_path().'/storage/importer/extracted/AllSets-x.json'), true);
+        //dd($json["JOU"]);
+        return ("Please come back later.");
     }
 
 }
